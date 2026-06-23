84ee5f0193d127c507f04e3924a3380f662d06ca
saij2 <137976038+saij2@users.noreply.github.com>
2026-06-22 18:58:10 -0700

    Create list input from user.py

---
+++ list input from user.py
@@ -0,0 +1,6 @@
+n = int(input("Enter number of items you want to add: "))
+values = [] # length = 0 
+for i in range(n):
+    number = int(input("Enter a number: "))
+    values.append(number) # add the element at the end of the list 
+print(values)
