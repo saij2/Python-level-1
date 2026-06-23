6cbbc209570c739bda7332a6dbf022c65236a376
saij2 <137976038+saij2@users.noreply.github.com>
2026-06-22 18:57:20 -0700

    Create list of functions and accesing loops.py

---
+++ list of functions and accesing loops.py
@@ -0,0 +1,21 @@
+ages = [10,12,13,14,15]
+ages[4] = 17
+# print(max(ages))
+# print(min(ages))
+# print(sum(ages))
+# print(sum(ages)/len(ages))
+# range(start,stop,step)
+"""
+start -> optional , default is 0 
+step -> optional , default is 1 
+
+
+step -> how much you need add to the current number to 
+get the next number 
+stop -> where to stop 
+"""
+# for i in range(len(ages)):# 0 to n-1 
+#     print(ages[i])
+# print(list(range(1,10)))
+numbers = list(range(1,101))
+print(len(numbers))  
