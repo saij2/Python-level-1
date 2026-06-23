35a42b128da21edbb67f54b151994db15806a6ba
saij2 <137976038+saij2@users.noreply.github.com>
2026-06-22 18:56:48 -0700

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,29 @@
+# 90 - 100 students 
+# name1 = "sashi"
+# name2 = "ram"
+# .....
+# name100 = "ravi"
+# Too many variables and it takes so much manual effort 
+# score1 = 60 
+# score2 = 55 
+# ....
+# score100 = 59 
+# max score , min score , sum , average manually 
+# Data structure is the way we can organise the data 
+# scores = [10,20,30,40] # you are creating a list 
+# 0 - 3 is the valid index range for length 4 
+# print(scores[0])
+# print(scores[1])
+# print(scores[2])
+# print(scores[3])
+# print(scores[-1])
+# print(scores[-4]) # -1 to -4 
+# length of list = 10 
+# valid positive indexes = 0 to 9 
+# valid negative indexes = -1 to -10 
+# length of list = n 
+# valid positive indexes = 0 to n-1 
+# valid negative indexes = -1 to -n 
+random = ["string",10,3.4,True] # heterogenous -> can store different data 
+# homogenous means same data 
+print(random)
