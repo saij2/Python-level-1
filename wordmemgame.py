4aa0f5733813f234f03096688dd3e4e0afcfc7e6
saij2 <137976038+saij2@users.noreply.github.com>
2026-06-29 18:51:14 -0700

    Create wordmemgame.py

---
+++ wordmemgame.py
@@ -0,0 +1,67 @@
+import random
+import os
+import time
+
+words = [
+    "apple", "banana", "orange", "grape", "mango",
+    "car", "house", "phone", "computer", "school"
+]
+
+print("""
+Difficulty:
+1. Easy - 4 words
+2. Medium - 5 words
+3. Hard - 6 words
+""")
+
+choice = int(input("Choose Difficulty (1-3): "))
+
+if choice == 3:
+    n = 6
+    difficulty = "Hard"
+elif choice == 2:
+    n = 5
+    difficulty = "Medium"
+else:
+    n = 4
+    difficulty = "Easy"
+
+
+selected_words = random.sample(words, n)
+
+time.sleep(1)
+os.system("clear")
+
+
+for word in selected_words:
+    print(word)
+    time.sleep(1)
+    os.system("clear")
+
+print("Memory Tester")
+print("Difficulty:", difficulty)
+
+
+guesses = input("Enter the words separated by commas: ").lower().split(",")
+
+
+'''guesses = [word.strip() for word in guesses]'''
+
+
+correct = 0
+
+for i in range(len(guesses), n):
+    if guesses[i] == selected_words[i]:
+        correct += 1
+
+
+score = (correct / n) * 100
+'''
+print("\nOriginal sequence:")
+print(", ".join(selected_words))
+
+print("\nYour answers:")
+print(", ".join(guesses))
+'''
+
+print(f"Score: {score:.0f}%")
